b6e8095d0fe54f92cfd00e49bd716a34d200e8f1
Sufiandy Elmy <elmysufiandy09@gmail.com>
2020-07-02 17:13:09 +0700

    Initial Commit

---
+++ BarCharts/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BarCharts
+//
+//  Created by Sufiandy Elmy on 02/07/20.
+//  Copyright © 2020 Sufiandy Elmy. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
